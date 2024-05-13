aa5e3d2991ea04c6b6d9fb1411e0f6a9d9d6b826
EventGamer67 <islamoffdanil67@gmail.com>
2024-05-13 12:23:16 +0500

    1

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
     #soup: BeautifulSoup = BeautifulSoup(html, 'html.parser')
     sup = initSupabase()
     data = Data(sup=sup)
-    date = datetime.datetime(2024,5,6)
-    link = "https://www.uksivt.ru//storage/files/all/ZAMENY/%D0%BC%D0%B0%D0%B9/06.05.24%D0%B3..pdf"
+    #date = datetime.datetime(2024,5,6)
+    #link = "https://www.uksivt.ru//storage/files/all/ZAMENY/%D0%BC%D0%B0%D0%B9/06.05.24%D0%B3..pdf"
     #link, date = getLastZamenaLink(soup=soup)
-    parse(link, date, sup)
+    #parse(link, date, sup)
 
 
-    #parseParas('29.04', date=datetime.datetime(2024, 4, 29), sup=sup, data=data)
+    parseParas('13.05', date=datetime.datetime(2024, 5, 13), sup=sup, data=data)
     pass
 
 
