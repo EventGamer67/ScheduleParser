8d06e3eac3b31833d16a9beeb38b7fa0a14f6f70
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-04 14:38:16 +0500

    test

--- parser/tasks.py
+++ parser/tasks.py
@@ -4,14 +4,14 @@ import asyncio
 import functools
 
 
-def sync(f):
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        return loop.run_until_complete(f(*args, **kwargs))
-
-    return wrapper
+# def sync(f):
+#     @functools.wraps(f)
+#     def wrapper(*args, **kwargs):
+#         loop = asyncio.new_event_loop()
+#         asyncio.set_event_loop(loop)
+#         return loop.run_until_complete(f(*args, **kwargs))
+#
+#     return wrapper
 
 
 @parser_celery_app.task
@@ -20,9 +20,8 @@ def get_latest_zamena_link():
 
 
 @parser_celery_app.task
-@sync
-async def get_latest_zamena_link_telegram(chat_id):
-    return await methods.get_latest_zamena_link_telegram(chat_id)
+def get_latest_zamena_link_telegram(chat_id):
+    asyncio.run(methods.get_latest_zamena_link_telegram(chat_id))
 
 
 @parser_celery_app.task
